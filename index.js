7d1d2ecc9e690fcd83c8f206536dbbc4a9f2181d
Deepak Kamat <depy45631@gmail.com>
2016-07-19 16:00:21 +0530

    Created the main index.js file for the program

---
+++ index.js
@@ -0,0 +1,7 @@
+/*------------------------------------------------------------------------------------
+
+ More details to be added soon
+
+ | Author   : Deepak Kamat & Aumkar Thakur
+ | Git Repo : https://github.com/PokemonGoApps/pokemongo-nearby-players-api
+-------------------------------------------------------------------------------------*/
