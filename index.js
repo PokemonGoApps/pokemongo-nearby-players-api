9131e606c6cc1e2e6c25e8dcbf3f351d7667afe9
Deepak Kamat <depy45631@gmail.com>
2016-07-22 17:54:03 +0530

    API work initiated.

--- index.js
+++ index.js
@@ -5,3 +5,91 @@
  | Author   : Deepak Kamat & Aumkar Thakur
  | Git Repo : https://github.com/PokemonGoApps/pokemongo-nearby-players-api
 -------------------------------------------------------------------------------------*/
+
+// Dependencies and requirements
+var _          = require('lodash'),
+    express    = require('express'),
+    request    = require('request'),
+    config     = require('./private/configs/config.js');
+
+// Server port
+var _port_ = process.env.PORT || 3000;
+
+// Keys
+var keys = {
+	geocodeApiKey: "AIzaSyDCsK09IQUZnU7AwM5gOhSOeugTNe-WamQ"
+};
+
+
+/*-----------------------------------------------------------------------------
+ExpressJS App - Settings and Routes
+------------------------------------------------------------------------------*/
+
+var pokemonGoApi = express(); 
+pokemonGoApi.use( '/static', express.static(__dirname + '/static') );
+pokemonGoApi.set( 'views', './views' );
+pokemonGoApi.set( 'view engine', 'pug' );
+
+
+/*///////////////
+ API Routes
+////////////////*/
+
+pokemonGoApi.get('/api/', function(req, res){
+	res.render('default');
+});
+
+pokemonGoApi.get('/api/near/:latitude/:longtitude', function(req, res){
+	// Set response headers here
+	res.set( {'Content-Type': 'text/json'} );
+
+	// Check if Latitude & Longtitude are correct
+	if (true) {}
+	
+	// Create a new object with the longtitude and latitude
+    var coords = new Coords( 
+    	req.params.latitude, 
+    	req.params.longtitude 
+    );
+
+
+    // Reverse Geocode
+    var rvrs_geocode_uri = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ coords.lat +","+ coords.lng +"&key="+ keys.geocodeApiKey + " ";
+
+    request(rvrs_geocode_uri, ( err, response, body ) => {
+    	console.log('::: Body :::');
+    	res.send( body );
+    });
+
+
+    // Send the response
+    // res.send( coords.float() );
+
+});
+
+
+
+/* Co-ordinates object */
+function Coords(latitude, longtitude) {
+	   this.lat = latitude;
+	   this.lng = longtitude;
+}
+
+Coords.prototype.float = function( precision ) {
+	var PRECISION = precision || 8;
+
+	var returnData = {
+		lat: parseFloat(this.lat).toPrecision( PRECISION ),
+		lng: parseFloat(this.lng).toPrecision( PRECISION )
+	};
+	
+	return returnData;
+}
+
+
+
+/*-----------------------------------------------------------------------------
+Serve the app hot
+------------------------------------------------------------------------------*/
+pokemonGoApi.listen( _port_ );
+console.log('Server is running at 127.0.0.1:' + _port_);
---
+++ private/oauth.js
@@ -0,0 +1,7 @@
+var authIds = {
+	google : {
+		clientID: '484493403653-le51gdgtsj3a00sojunca8eujngpipjv.apps.googleusercontent.com',
+		clientSecret: '9dSCnd0p4CKgSufOBpvpgT1l',
+		callbackURL: '/auth/google/callback'
+	}
+};
